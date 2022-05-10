a20d7d6eb99d8b942b1cbcd0f5f0aa8d0c85d1ba
Clare MacAdie <clare@macadie.co.uk>
2022-05-10 18:48:49 +0100

    Added tests for login functionality. 3 of them are commented out because they don't work: test_view_signup_form_signed_in, test_signup_signed_out and test_signup_mismatched_passwords.

--- test/bbc_test.rb
+++ test/bbc_test.rb
@@ -20,13 +20,13 @@ class CMSTest < Minitest::Test
   
   end
 
-  # def session
-  #   last_request.env["rack.session"]
-  # end
+  def session
+    last_request.env["rack.session"]
+  end
 
-  # def admin_session
-  #   { "rack.session" => { username: "admin" } }
-  # end
+  def admin_session
+    { "rack.session" => { username: "admin" } }
+  end
 
   def test_homepage
     get "/"
@@ -36,49 +36,49 @@ class CMSTest < Minitest::Test
     assert_includes last_response.body, "Welcome to the Book Borrowers' Collective."
   end
 
-  # def test_signin_form
-  #   get "/users/signin"
+  def test_signin_form
+    get "/users/signin"
 
-  #   assert_equal 200, last_response.status
-  #   assert_includes last_response.body, "<input"
-  #   assert_includes last_response.body, %q(<button type="submit")
-  # end
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "<input"
+    assert_includes last_response.body, %q(<button type="submit")
+  end
 
-  # def test_signin
-  #   post "/users/signin", username: "admin", password: "secret"
-  #   assert_equal 302, last_response.status
-  #   assert_equal "Welcome!", session[:message]
-  #   assert_equal "admin", session[:username]
+  def test_signin
+    post "/users/signin", username: "admin", password: "secret"
+    assert_equal 302, last_response.status
+    assert_equal "Welcome!", session[:message]
+    assert_equal "admin", session[:username]
 
-  #   get last_response["Location"]
-  #   assert_includes last_response.body, "Signed in as admin"
-  # end
+    get last_response["Location"]
+    assert_includes last_response.body, "Signed in as admin"
+  end
 
-  # def test_signin_with_bad_credentials
-  #   post "/users/signin", username: "guest", password: "shhhh"
-  #   assert_equal 422, last_response.status
-  #   assert_nil session[:username]
-  #   assert_includes last_response.body, "Invalid credentials"
-  # end
+  def test_signin_with_bad_credentials
+    post "/users/signin", username: "guest", password: "shhhh"
+    assert_equal 422, last_response.status
+    assert_nil session[:username]
+    assert_includes last_response.body, "Invalid credentials"
+  end
 
-  # def test_signout
-  #   get "/", {}, {"rack.session" => { username: "admin" } }
-  #   assert_includes last_response.body, "Signed in as admin"
+  def test_signout
+    get "/", {}, {"rack.session" => { username: "admin" } }
+    assert_includes last_response.body, "Signed in as admin"
 
-  #   post "/users/signout"
-  #   assert_equal "You have been signed out", session[:message]
+    post "/users/signout"
+    assert_equal "You have been signed out", session[:message]
 
-  #   get last_response["Location"]
-  #   assert_nil session[:username]
-  #   assert_includes last_response.body, "Sign In"
-  # end
+    get last_response["Location"]
+    assert_nil session[:username]
+    assert_includes last_response.body, "Sign In"
+  end
   
-  # def test_view_signup_form_signed_out
-  #   get "/users/signup"
+  def test_view_signup_form_signed_out
+    get "/users/signup"
     
-  #   assert_equal 200, last_response.status
-  #   assert_includes last_response.body, "Reenter password"
-  # end
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "Reenter password"
+  end
   
   # def test_view_signup_form_signed_in
   #   get "/users/signup", {}, admin_session
@@ -97,19 +97,19 @@ class CMSTest < Minitest::Test
   #   assert_includes last_response.body, "Signed in as joe."
   # end
   
-  # def test_signup_signed_in
-  #   post "/users/signup", {new_username: "joe", password: "dfghiewo34334", reenter_password: "dfghiewo34334"}, admin_session
+  def test_signup_signed_in
+    post "/users/signup", {new_username: "joe", password: "dfghiewo34334", reenter_password: "dfghiewo34334"}, admin_session
     
-  #   assert_equal 302, last_response.status
-  #   assert_equal "You must be signed out to do that.", session[:message]
-  # end
+    assert_equal 302, last_response.status
+    assert_equal "You must be signed out to do that.", session[:message]
+  end
   
-  # def test_signup_existing_username
-  #   post "/users/signup", {new_username: "admin", password: "dfghiewo34334", reenter_password: "dfghiewo34334"}
+  def test_signup_existing_username
+    post "/users/signup", {new_username: "admin", password: "dfghiewo34334", reenter_password: "dfghiewo34334"}
     
-  #   assert_equal 422, last_response.status
-  #   assert_includes last_response.body, "That username already exists."
-  # end
+    assert_equal 422, last_response.status
+    assert_includes last_response.body, "That username already exists."
+  end
 
   # def test_signup_mismatched_passwords
   #   post "/users/signup", {new_username: "joe", password: "dfghiewo34334", reenter_password: "mismatched"}
