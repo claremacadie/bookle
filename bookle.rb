faff42233dd2618fa518cd7043e288708d212b67
Clare MacAdie <clare@macadie.co.uk>
2022-05-23 14:28:20 +0100

    Added jquery for signout and changed jquery message for deleting a book.

--- bookle.rb
+++ bookle.rb
@@ -160,7 +160,11 @@ post "/users/signout" do
   session.delete(:user_name)
   session.delete(:user_id)
   session[:message] = "You have been signed out"
-  redirect "/"
+  if env["HTTP_X_REQUESTED_WITH"] == "XMLHttpRequest"
+	  "/"
+	else
+	  redirect "/"
+  end
 end
 
 get "/users/signup" do
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -8,3 +8,25 @@ $(function() {
     }
   });
 });
+
+$(function() {
+  $("form.delete_book").submit(function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    var ok = confirm("Are you sure you want to delete this book? This cannot be undone!");
+    if (ok) {
+      this.submit();
+    }
+  });
+});
+
+$(function() {
+  $("form.signout").submit(function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    var ok = confirm("Are you sure you want to sign out?");
+    if (ok) {
+      this.submit();
+    }
+  });
+});
